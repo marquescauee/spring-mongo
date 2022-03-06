efa627a2eb3c609d3306cc5ea936427a1b2ef598
marquescauee <cauelopesmarques123@gmail.com>
2022-03-06 19:13:20 -0300

    Entity e Controller User

---
+++ src/main/java/br/com/cauelopesmarques/springmongo/controllers/UserController.java
@@ -0,0 +1,28 @@
+package br.com.cauelopesmarques.springmongo.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cauelopesmarques.springmongo.domain.User;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Maria", "maria@gmail.com");
+		User alex = new User("2", "Alex", "alex@gmail.com");
+		
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		
+		return ResponseEntity.ok().body(list);
+	}
+}
