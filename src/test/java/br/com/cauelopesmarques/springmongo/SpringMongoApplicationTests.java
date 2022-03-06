a775dd3deb378d851a9af93d4a0974bcea8b3707
marquescauee <cauelopesmarques123@gmail.com>
2022-03-06 18:53:52 -0300

    First commit

---
+++ src/test/java/br/com/cauelopesmarques/springmongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cauelopesmarques.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
