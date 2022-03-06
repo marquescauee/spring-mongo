871260ecd39dddd9ef02704aeda61d4b5375d82b
marquescauee <cauelopesmarques123@gmail.com>
2022-03-06 20:29:48 -0300

    Atualizacao de usuario

--- src/main/java/br/com/cauelopesmarques/springmongo/services/UserService.java
+++ src/main/java/br/com/cauelopesmarques/springmongo/services/UserService.java
@@ -36,6 +36,18 @@ public class UserService {
 		ur.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		
+		return ur.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
