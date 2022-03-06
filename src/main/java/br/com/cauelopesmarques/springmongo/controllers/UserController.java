5d7a872432a4409355ecc1393bc81402289e49d3
marquescauee <cauelopesmarques123@gmail.com>
2022-03-06 20:05:50 -0300

    Obtendo usuario por ID

--- src/main/java/br/com/cauelopesmarques/springmongo/controllers/UserController.java
+++ src/main/java/br/com/cauelopesmarques/springmongo/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,12 @@ public class UserController {
 		List<UserDTO> listDTO = list.stream().map(e -> new UserDTO(e)).collect(Collectors.toList());
 		
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = us.findById(id);	
 		
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/br/com/cauelopesmarques/springmongo/services/UserService.java
+++ src/main/java/br/com/cauelopesmarques/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.cauelopesmarques.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.cauelopesmarques.springmongo.domains.User;
 import br.com.cauelopesmarques.springmongo.repositories.UserRepository;
+import br.com.cauelopesmarques.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return ur.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = ur.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontado"));
 	}
 }
