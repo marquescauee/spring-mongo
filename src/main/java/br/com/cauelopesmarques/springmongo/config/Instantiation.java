e27cb6e2484f8975a4aabc794acb3c0bf27df4fe
marquescauee <cauelopesmarques123@gmail.com>
2022-03-06 22:00:44 -0300

    Acrescentando comentários aos posts

--- src/main/java/br/com/cauelopesmarques/springmongo/config/Instantiation.java
+++ src/main/java/br/com/cauelopesmarques/springmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.cauelopesmarques.springmongo.domains.Post;
 import br.com.cauelopesmarques.springmongo.domains.User;
 import br.com.cauelopesmarques.springmongo.dto.AuthorDTO;
+import br.com.cauelopesmarques.springmongo.dto.CommentDTO;
 import br.com.cauelopesmarques.springmongo.repositories.PostRepository;
 import br.com.cauelopesmarques.springmongo.repositories.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP, Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 	
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+		
 		pr.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/br/com/cauelopesmarques/springmongo/domains/Post.java
+++ src/main/java/br/com/cauelopesmarques/springmongo/domains/Post.java
@@ -1,13 +1,16 @@
 package br.com.cauelopesmarques.springmongo.domains;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.cauelopesmarques.springmongo.dto.AuthorDTO;
+import br.com.cauelopesmarques.springmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -30,6 +34,10 @@ public class Post implements Serializable {
 		this.title = title;
 		this.body = body;
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	public AuthorDTO getAuthor() {
